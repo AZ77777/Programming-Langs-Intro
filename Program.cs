59a69eb0abd36697624db9a534576b6a7b1dbb19
Alexey Zernov <discocat77@gmail.com>
2022-09-12 15:34:42 +0300

    added task19

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter 5-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CheckifPalindrom(number);
+
+void CheckifPalindrom(int number)
+{
+    if(number >= 10000)
+    {
+        int division1 = number / 10000;
+        int remainder1 = number % 10;
+ 
+            if(division1 == remainder1)
+            {
+                number = number / 10;
+                int division2 = number / 100;
+                int remainder2 = number % 10;
+                if(division2 == remainder2)
+                    Console.WriteLine("Yes");
+            }
+            else 
+            Console.WriteLine("No");
+            
+    }
+    else
+    Console.WriteLine("Incorrect number!");
+}
